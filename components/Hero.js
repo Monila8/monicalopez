3d59a9eb76fb830515f9499157d00149a19143d2
Monila8 <monicalopezarroyave@gmail.com>
2023-12-25 18:41:15 -0500

    hero styles

--- components/Hero.js
+++ components/Hero.js
@@ -1,38 +1,37 @@
 import Image from "next/image"
-// import '../App.css'
 
 export default function Hero() {
   return (
     <div className="hero__wrapper">
       <h2 className="hero__title">Home</h2>
-      <div className="hero__content">
-        <div className="hero__text">
-          <h3 className="hero__preheadline">Hello! I&apos;m</h3>
-          <h2 className="hero__headline">Mónica López</h2>
-          <p>Software developer &amp; Frontend</p>
-          <button
+        <div className="hero__content">
+          <div className="hero__text">
+            <h3 className="hero__preheadline">Hello! I&apos;m</h3>
+            <h2 className="hero__headline">Mónica López</h2>
+            <p>Software developer &amp; Frontend</p>
+            <button
             href="#resume"
             className="hero__download-button">
             Get Resume
-            <Image
+              <Image
               src="/images/download.png"
               className="hero__download-icon"
               width={15}
               height={15}
               alt="Me"
-            />
-          </button>
-        </div>
-        <div className="hero__portrait">
-          <Image
+              />
+            </button>
+          </div>
+          <div className="hero__portrait">
+            <Image
             className="hero__image"
             src="/images/me.png"
             width={400}
             height={600}
             alt="Me"
-          />
+           />
+          </div>
         </div>
       </div>
-    </div>
   )
 }
