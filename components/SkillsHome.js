65cf4f5c3b1a176d456aed3ece91f1bf12088df0
Monila8 <monicalopezarroyave@gmail.com>
2023-12-28 20:52:57 -0500

    skillsHome styles

--- components/SkillsHome.js
+++ components/SkillsHome.js
@@ -1,4 +1,3 @@
-// import '../App.css'
 import assetsURLs from "./Constants"
 import Image from "next/image"
 
@@ -10,7 +9,10 @@ export default function SkillsHome() {
       <div className="skillsHome__title">
         <h1>Skills</h1>
       </div>
-      <div className="skillsHome__coding-icon">
+      <div className="skillsHome__development">
+        <div className="skillsHome__coding-icon"></div>
+        <br />
+        <h4 className="skillsHome__title">Development </h4>
         <Image
           src="/images/coding-icon.webp"
           className="skillsHome__download-icon"
@@ -18,67 +20,65 @@ export default function SkillsHome() {
           height={30}
           alt="coding-icon"
         />
-      </div>
-      <br />
-      <h4 className="skillsHome__title">Development </h4>
-      <div className="skillsHome__col-12">
-        <div className="skillsHome__icon">
-          <Image
-            src={htmlIcon}
-            width={80}
-            height={80}
-            alt="html icon"
-          />
-          <p className="skillsHome__text">HTML</p>
+        <div className="skillsHome__col-12">
+          <div className="skillsHome__icon">
+            <Image
+              src={htmlIcon}
+              width={80}
+              height={80}
+              alt="html icon"
+            />
+            <p className="skillsHome__text">HTML</p>
+          </div>
+          <div className="skillsHome__icon">
+            <Image
+              src={cssIcon}
+              width={80}
+              height={80}
+              alt="css icon"
+            />
+            <p className="skillsHome__text">CSS</p>
+          </div>
+          <div className="skillsHome__icon">
+            <Image
+              src={javascriptIcon}
+              width={80}
+              height={80}
+              alt="js icon"
+            />
+            <p className="skillsHome__text">JavaScript</p>
+          </div>
         </div>
-        <div className="skillsHome__icon">
-          <Image
-            src={cssIcon}
-            width={80}
-            height={80}
-            alt="css icon"
-          />
-          <p className="skillsHome__text">CSS</p>
+        <div className="skillsHome__col-12">
+          <div className="skillsHome__icon">
+            <Image
+              src={reactIcon}
+              width={80}
+              height={80}
+              alt="react icon"
+            />
+            <p className="skillsHome__text">React</p>
+          </div>
+          <div className="skillsHome__icon">
+            <Image
+              src={reduxIcon}
+              width={80}
+              height={80}
+              alt="redux icon"
+            />
+            <p className="skillsHome__text">Redux</p>
+          </div>
         </div>
-        <div className="skillsHome__icon">
-          <Image
-            src={javascriptIcon}
-            width={80}
-            height={80}
-            alt="js icon"
-          />
-          <p className="skillsHome__text">JavaScript</p>
-        </div>
-      </div>
-      <div className="skillsHome__col-12">
-        <div className="skillsHome__icon">
-          <Image
-            src={reactIcon}
-            width={80}
-            height={80}
-            alt="react icon"
-          />
-          <p className="skillsHome__text">React</p>
-        </div>
-        <div className="skillsHome__icon">
-          <Image
-            src={reduxIcon}
-            width={80}
-            height={80}
-            alt="redux icon"
-          />
-          <p className="skillsHome__text">Redux</p>
-        </div>
-      </div>
-      <div className="skillsHome__col-12">
-        <div className="skillsHome__icon">
-          <Image
-            src={nextIcon}
-            width={80}
-            height={80}
-            alt="next icon"
-          />
-          <p className="skillsHome__text">Next.js</p>
+        <div className="skillsHome__col-12">
+          <div className="skillsHome__icon">
+            <Image
+              src={nextIcon}
+              width={80}
+              height={80}
+              alt="next icon"
+            />
+            <p className="skillsHome__text">Next.js</p>
+          </div>
         </div>
       </div>
     </div>
